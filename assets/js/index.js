bc2d3140612402cd9f79883f2e46e7338b7cb4ff
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2023-12-28 22:16:39 +0200

    clean code a little

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,8 @@ taskForm.addEventListener('submit', (e) => {
 
   const newTaskObj = createTaskObj(taskText);
   todos.push(newTaskObj);
-  console.log(todos);
+
+  console.log(todos); //TODO delete
 
   const newTask = createTask(newTaskObj);
   taskContainer.append(newTask);
@@ -60,19 +61,14 @@ function createTask(taskObj) {
   taskDeleteBtn.addEventListener('click', (e) => {
     const taskToRemove = e.currentTarget.parentElement;
     const taskToRemoveId = taskToRemove.dataset.id;
-    // console.log(taskToRemoveId);
 
     taskToRemove.remove();
-    todos = todos.filter((el) => {
-      // console.log(el.id, taskToRemoveId);
-      return el.id !== +taskToRemoveId;
-    });
-    console.log(todos);
+    todos = todos.filter((el) => el.id !== +taskToRemoveId);
+    console.log(todos); //TODO delete
   });
 
   const trashIcon = document.createElement('i');
   trashIcon.classList.add('far', 'fa-trash-alt');
-
   taskDeleteBtn.append(trashIcon);
 
   task.append(taskParagraph);
@@ -80,28 +76,3 @@ function createTask(taskObj) {
 
   return task;
 }
-
-// taskContainer.addEventListener('click', (e) => {
-//   if (e.target.tagName === 'BUTTON') {
-//     e.stopPropagation();
-//   }
-//   if (!e.target.classList.contains('deleteTaskBtn')) return;
-//   // if (e.target.tagName !== 'BUTTON' || !e.target.classList.contains('deleteTaskBtn'))
-//   //   return;
-//   console.log('hello');
-//   e.target.parentElement.remove();
-// });
-
-//==============================
-
-// Просунутий рівень
-// Створити глобальну змінну todos яка буде пустим масивом.
-// При створенні задачі також необхідно створювати об'єкт, який буде містити
-// поточні дані про цю конкретну задачу (її текст, чи виконана вона і т.д)
-// і покласти цю задачу у массив todos.
-// При видаленні задачі зі сторінки необхідно також видалити її з масиву.
-
-// Max рівень
-// Додати кожній задачі можливість відстеження того, чи виконана вона чи
-// ні у її об'єкті і на сторінці. По кліку на текст задачи або на  інпут-чекбокс
-// (якщо такий буде) завдання має змінювати дані про виконаність. Якщо
