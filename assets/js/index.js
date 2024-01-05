0b480cb09b87989e47024c5435b65b8cde3a5224
Mike <misha.grigorov2020@gmail.com>
2024-01-05 17:53:48 +0200

    promise deep copy

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,6 +86,11 @@
 
 // console.log();
 
+/**
+ *
+ * @param {*} [i=1]
+ * @returns
+ */
 function HW1(i = 1) {
   if (i > 10) return;
   setTimeout(() => {
@@ -95,17 +100,55 @@ function HW1(i = 1) {
 }
 
 function HW2() {
-  let counter = (
-    (i = 0) =>
-    () =>
-      ++i
-  )();
+  let i = 0;
   const intervalId = setInterval(() => {
-    const i = counter();
+    i++;
     console.log(i);
     if (i === 10) clearInterval(intervalId);
   }, 1000);
-  // setTimeout(() => {
-  //   clearInterval(intervalId);
-  // }, 10000);
 }
+
+const users = [
+  { id: 0, balance: 900 },
+  { id: 1, balance: 500 },
+  { id: 2, balance: 100 },
+  { id: 2, balance: 800 },
+];
+
+// const shallowCopy = users;
+// shallowCopy[0] = { hello: 'hello' };
+// console.log(shallowCopy, users, users[0] === shallowCopy[0]);
+
+// const shallowCopySpread = [...users];
+// shallowCopySpread[0] = { hello: 'hello' };
+// console.log(
+//   shallowCopySpread,
+//   users,
+//   users[0] === shallowCopySpread[0],
+//   users[1] === shallowCopySpread[1]
+// );
+
+// const shallowCopySpread = JSON.parse(JSON.stringify(users));
+const shallowCopySpread = structuredClone(users);
+shallowCopySpread[0] = { hello: 'hello' };
+console.log(
+  shallowCopySpread,
+  users,
+  users[0] === shallowCopySpread[0],
+  users[1] === shallowCopySpread[1]
+);
+
+//================
+
+// const promise = new Promise((res, rej) => {
+//   res({ hello: 'hello' });
+//   rej();
+// }).then((result) => {
+//   console.log(result);
+// });
+
+// const showStatus = (status) => console.log(status);
+
+// new Promise((res, rej) => {
+//   Math.random() >= 0.5 ? res('good') : rej('bad');
+// }).then(showStatus, showStatus);
